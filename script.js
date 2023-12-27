080f9d93fa4a29eba54b1aa5b1c5c87fe32433dd
Paul Fiesel <paul.fiesel@gmail.com>
2023-12-27 17:10:04 -0600

    Updated script.js

--- script.js
+++ script.js
@@ -155,11 +155,15 @@ function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
   monsterStats.style.display = "block";
-
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {
-
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeapon].power;
 }
 
 function dodge() {
